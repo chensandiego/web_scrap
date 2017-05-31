719ae34ca945c272245ebda2b877dc2d5b13df93
chen <chensandiego@gmail.com>
2017-05-31 14:27:13 +0800

    first

---
+++ scrap_1.py
@@ -0,0 +1,33 @@
+import urllib.request
+from urllib.error import URLError,HTTPError,ContentTooShortError
+import re
+
+def download(url,user_agent='wswp',num_retries=2,charset='utf-8'):
+    print('downloading:',url)
+    request=urllib.request.Request(url)
+    request.add_header('User-agent',user_agent)
+    try:
+        resp=urllib.request.urlopen(request)
+        cs=resp.headers.get_content_charset()
+        if not cs:
+            cs=charset
+        html=resp.read().decode(cs)
+    except (URLError,HTTPError,ContentTooShortError) as e:
+        print('download errorL',e.reason)
+
+        html=None
+
+        if num_retries >0:
+            if hasattr(e,'code') and 500<=e.code <600:
+                return download(url,num_retries-1)
+    return html
+
+
+def crawl_sitemap(url):
+    sitemap=download(url)
+    links=re.findall('<loc>(.*?)</loc>',sitemap)
+    for link in links:
+        html=download(link)
+
+if __name__=='__main__':
+    crawl_sitemap('http://example.webscraping.com/sitemap.xml')
