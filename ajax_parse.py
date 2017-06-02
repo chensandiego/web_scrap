68543f26851d3bff519c8b15d842024fce6da234
chen <chensandiego@gmail.com>
2017-06-02 12:17:00 +0800

    add redirect detect

---
+++ ajax_parse.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+
+import time
+
+driver=webdriver.PhantomJS()
+driver.get("http://pythonscraping.com/pages/javascript/ajaxDemo.html")
+
+
+time.sleep(3)
+
+
+print(driver.find_element_by_id("content").text)
+driver.close()
